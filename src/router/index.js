45a9365a1a811569c8ff65b77a0159e81ea40aa6
建維 <4a990010@stust.edu.tw>
2025-10-31 22:43:53 +0800

    fix:createWebHashHistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 import About from '../views/About.vue';
 import Project from '../views/Project.vue';
